f2e804e6fe9a7e1960d5bde779a46aedbe9277b0
marcoddm <marantal2013@hotmail.com>
2022-08-25 21:56:52 -0300

    que fiaca estos mensajes

--- src/App.js
+++ src/App.js
@@ -1,20 +1,31 @@
 import React, {useEffect, useState} from 'react';
+import Characters from "./Components/Characters.js";
 import Navbar from "./Components/Navbar.js";
-
-
-
+import Pagination from './Components/Pagination.js';
 
 function App() {
 
   const [characters, setCharacters] = useState([])
+  const [info, setinfo] = useState({})
   const initialUrl = "https://rickandmortyapi.com/api/character";
 
   const fetchCharacters = (url) => {
     fetch(url)
     .then(response => response.json())
-    .then(data => setCharacters(data.results))
+    .then(data => {
+      setCharacters(data.results);
+      setinfo(data.info)
+    } )
     .catch(err => console.log(err))
   };
+
+  const onPrevious = () => {
+    fetchCharacters(info.prev);
+  }
+
+  const onNext = () => {
+    fetchCharacters(info.next);
+  }
 
   useEffect(() => {
     fetchCharacters(initialUrl);
@@ -23,7 +34,10 @@ function App() {
   return (
     <>
         <Navbar brand="Rick and Morty"/>
-        <div className="container">
+        <div className="container mt-5 mb-5">
+        <Pagination prev={info.prev} next={info.next} onPrevious={onPrevious} onNext={onNext}/>
+        <Characters characters={characters}/>
+        <Pagination prev={info.prev} next={info.next} onPrevious={onPrevious} onNext={onNext}/>
         </div>
     </>
 
